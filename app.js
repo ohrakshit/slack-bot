e0fc090a7ddb045bc40f5c0221ab5555394eb616
Rakshit Kumar <48801964+astronaut99@users.noreply.github.com>
2022-05-03 15:07:38 +0530

    second commit

--- app.js
+++ app.js
@@ -2,31 +2,20 @@ require("dotenv").config({ path: __dirname + "/.env" });
 
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+//YOUR CODE HERE
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
 
+const token = process.env.BOT_TOKEN;
 const eventsApi = require("@slack/events-api");
-const slackEvents = eventsApi.createEventAdapter(
-  process.env.SLACK_SIGNING_SECRET
-);
-
-const token = process.env.SLACK_BOT_TOKEN;
-
+const slackEvents = eventsApi.createEventAdapter(process.env.SIGNING_SECRET);
 const { WebClient, LogLevel } = require("@slack/web-api");
 const client = new WebClient(token, {
   logLevel: LogLevel.DEBUG,
 });
-
 app.use("/", slackEvents.expressMiddleware());
-
 slackEvents.on("message", async (event) => {
   console.log(event);
-  const channel = event.channel;
-  if (!event.bot_profile) {
-    client.chat.postMessage({ channel, token, text: "Test recieved!" });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`);
 });
